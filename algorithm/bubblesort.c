3ba0969117a0ee1493df9b1f8a38983d5d91c5fb
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-05-08 11:34:59 +0800

    Create bubblesort.c

---
+++ algorithm/bubblesort.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  bubblesort
+//
+//  Created by xuhuan lu on 2022/5/7.
+//
+
+#include <stdio.h>
+#define LEN 10
+
+int main(int argc, const char * argv[]) {
+    int i, arr[LEN];
+    
+    for (i = 0; i < LEN; i++)
+        scanf("%d", &arr[i]);
+    
+    for (i = 0; i < LEN - 1; i++)
+        for (int j = 0; j < LEN - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    
+    for (i = 0; i < LEN; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
