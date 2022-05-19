4c55f44b76a2d0403aaa60871f3af7568af15649
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-05-19 16:39:02 +0800

    Create name_case.py

---
+++ python/name_case.py
@@ -0,0 +1,22 @@
+
+# 练习2-3：个性化信息
+name = "Eric"
+message = f"Hello, {name.title()}, would you like to learn some Python today?"
+print(message)
+
+
+# 练习2-4：调整名字的大小
+name1 = "eRic"
+name2 = "ERIC"
+print(name.upper())
+print(name2.title())
+print(name1.lower())
+
+# 练习2-5：名言
+saying = "A person who never made a mistake never tried anything new."
+name3 = "Alber Einstein"
+print(f'{name3} said: "{saying}"')
+
+# 练习2-6：名言2
+message = f'{name3} said: "{saying}"'
+print(message)
