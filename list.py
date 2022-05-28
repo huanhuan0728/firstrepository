cba24000123f4eb7b4a87aecb0b38885138e4f3d
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-05-28 23:22:25 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,22 @@
+# 第三章练习题
+
+
+where = ['shanghai', 'beijing', 'guanzhou', 'kunming']
+print(f'您想去的地方是{where}')
+
+where.sort()
+print(f'sort:您想去的地方是{where}')
+
+where = ['shanghai', 'beijing', 'guanzhou', 'kunming']
+
+print('here is original list')
+print(where)
+
+print('\nhere is the sorted list')
+print(sorted(where))
+
+# 倒着打印表
+where.reverse()
+print(where)
+
+
