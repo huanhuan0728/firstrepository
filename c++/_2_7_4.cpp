61bc3ee7f3b427baf880daa7f9f6d6d63970730a
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-06-11 20:06:41 +0800

    Create _2_7_4.cpp

---
+++ c++/_2_7_4.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  _2_7_4
+//
+//  Created by xuhuan lu on 2022/6/11.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    int old;
+    cin >> old;
+    cout << "your old is " << old*12 << " "month old ";
+    
+    return 0;
+}
