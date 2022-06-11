8a4b42881ecb0747dc1640b16564bfed55a13bd2
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-06-11 20:13:04 +0800

    Create _2_7_7.cpp

---
+++ c++/_2_7_7.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  _2_7_7
+//
+//  Created by xuhuan lu on 2022/6/11.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int hours, minutes;
+    
+    cout << "Please enter of hours: ";
+    cin >> hours;
+    cout << "Please enter of minutes: ";
+    cin >> minutes;
+    
+    cout << "Time: " << hours << ":" << minutes << endl;
+    
+    return 0;
+}
