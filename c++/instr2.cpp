6b6714f3e13669a3b4fc4e340b0b7ae3f55785d0
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-06-16 23:45:49 +0800

    Create instr2.cpp

---
+++ c++/instr2.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  instr2
+//
+//  Created by xuhuan lu on 2022/6/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+    
+    cout << "Enter your name:\n ";
+    cin.getline(name, ArSize);
+    cout << "Enter your favorite dessert:\n";
+    cin.getline(dessert, ArSize);
+    cout << "I have some delicious " << dessert;
+    cout << "for you " << name << ".\n";
+    
+    return 0;
+}
