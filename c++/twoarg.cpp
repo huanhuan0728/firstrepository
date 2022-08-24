83133fa4870f5f5463d406eddc44decc1d80fbe3
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-08-24 22:43:22 +0800

    Create twoarg.cpp

---
+++ c++/twoarg.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  twoarg
+//
+//  Created by xuhuan lu on 2022/8/23.
+//
+
+#include <iostream>
+using namespace std;
+void n_chars (char, int);
+
+int main(int argc, const char * argv[]) {
+    
+    int times;
+    char ch;
+    
+    cout << "Enter a character: ";
+    cin >> ch;
+    while (ch != 'q')   // q to quit
+    {
+        cout << "Enter an interger: ";
+        cin >> times;
+        n_chars(ch, times); // function with two arguments
+        cout << "\nEnter another character or press the"
+        "q-key to quit.";
+        cin >> ch;
+    }
+    
+    cout << "The value of times is " << times << ".\n";
+    cout << "Bye.\n";
+    
+    return 0;
+}
+
+void n_chars (char c, int n)
+{
+    while (n-- > 0)
+        cout << c;
+}
