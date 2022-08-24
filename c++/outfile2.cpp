5f5c26aa5d2d16bc613387353888164cf55eafb7
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-08-24 22:42:05 +0800

    Create outfile2.cpp

---
+++ c++/outfile2.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  //
+//  main.cpp
+//  outfile
+//
+//  Created by xuhuan lu on 2022/8/10.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    char automobile[50];
+    int year;
+    double a_price;
+    double d_price;
+    
+    ofstream outFile;   // create object for output
+    outFile.open("carinfo.txt");    // associate with a file
+    
+    cout << "Enter the make and model of automobile: ";
+    cin.getline(automobile, 50);
+    cout << "Enter the mobile year: ";
+    cin >> year;
+    cout << "Enter the original asking price: ";
+    cin >> a_price;
+    d_price = 0.913 * a_price;
+    
+//  display infomation on screen with cout
+    cout << fixed;
+    cout.precision(2);
+    cout.setf(ios_base::showpoint);
+    cout << "Make and model: " << automobile << endl;
+    cout << "Was asking $ " << a_price << endl;
+    cout << "Now asking & " << d_price <<endl;
+    
+// now do exact same things using outFile instead of cout
+    outFile << fixed;
+    outFile.precision(2);
+    outFile.setf(ios_base::showpoint);
+    outFile << "Make and model: " << automobile << endl;
+    outFile << "Year: " << year << endl;
+    outFile << "Was asking & " << a_price << endl;
+    outFile << "Nas asking & " << d_price << endl;
+
+    
+    return 0;
+}
+
