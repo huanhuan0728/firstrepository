cb8c8c2c8c7d664a41a56a9b332c10a8a99401fb
huanhuan0728 <102048940+huanhuan0728@users.noreply.github.com>
2022-11-10 23:51:41 +0800

    Update Queue_Sq.c

--- c/SqQueue/Queue_Sq.c
+++ c/SqQueue/Queue_Sq.c
@@ -17,20 +17,20 @@ ElemType InitQueue_Sq(SqQueue *Q, int Size){
         return ERROR;
     Q->maxSize = Size;
     Q->front = Q->rear = 0;
-    Q->length = 0;    
     return OK;
 }
 
 // 入队，在当前队列尾元素之后插入元素e为新队列元素
 ElemType EnQueue_Sq(SqQueue *Q, ElemType e){
-    if (Q->length == Q->rear)
+    if (Q->rear == Q->maxSize)
         return ERROR;
-    Q->rear = e;
-    Q->rear = (Q->rear+1) % Q->maxSize;
-    Q->elem[Q->rear] = e;   //入队
-    Q->length++;
-    
-    return OK;
+    else{
+        Q->elem[Q->rear] = e;   //入队
+        Q->rear = (Q->rear+1) % Q->maxSize;
+        Q->length++;
+        
+        return OK;
+    }
 }
 
 // 出队，若队列不空则删除当前队列Q中头元素，用e返回其值，并返回ok，否则返回error
@@ -51,11 +51,42 @@ int QueueLength_Sq(SqQueue *Q){
 
 // 判空
 bool QueueEmpty_Sq(SqQueue *Q){
-    if (Q->length == 0)
+    if (Q->rear == 0)
         return true;
     else
         return  false;
 }
+
+// 若队列不为空，则用e返回Q的队列头元素，并返回OK；否则返回ERROR
+ElemType GetHead_Sq(SqQueue *Q, ElemType *e){
+    if (QueueEmpty_Sq(Q))
+        return ERROR;
+    else{
+        *e = Q->elem[Q->front];
+        return OK;
+    }
+    
+}
+
+// 将队列销毁，使其不复存在
+ElemType DestroyQueue_Sq(SqQueue *Q){
+    Q->front = 0;
+    while (Q->elem[Q->front]){
+        free(&Q->elem[Q->front]);
+        Q->front++;
+    }
+    
+    return OK;
+}
+
+// 打印这个队列
+void PrintfQueue_Sq(SqQueue *Q){
+    for (int count = 0; count <= Q->rear; count++){
+        printf("%d ", Q->elem[count]);
+    }
+}
+
+
 
 
 
