0263aaa1028dd4509f93c0d754fb206732d10661
xuhuan lu <102048940+huanhuan0728@users.noreply.github.com>
2022-12-23 00:36:25 +0800

    Update function.c

--- c/课程设计/学生信息管理系统/function.c
+++ c/课程设计/学生信息管理系统/function.c
@@ -1,181 +1,105 @@
 //
-//  main.c
+//  function.c
 //  学生信息管理系统
 //
 //  Created by xuhuan lu on 2022/12/19.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "function.h"
-void showstudent(Item item);
-char * s_gets(char * st, int n);
-void menu(void);
-void Add(void);
-void Delete(List *plist);
-void Serch(List *plist);
-void Updet(List *plist);
-void show(void);
-List student;
-//Item temp;
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-//    List student;
-//    Item temp;
+void InitializeList(List * plist){
+    *plist = NULL;
+}
+
+//如果链表为空返回true，否则false
+bool ListIsEmpty(const List * plist){
+    if(*plist == NULL)
+        return true;
+    else
+        return false;
+}
+
+//如果链表已满，返回true
+bool ListIsFull(const List *plist){
+    Node *pt;
+    bool full;
     
-    //初始化
-    InitializeList(&student);
-    if(ListIsFull(&student)){
-        fprintf(stderr, "No memory is aviliable!");
-        exit(1);
+    pt = (Node*)malloc(sizeof(Node));
+    if(pt == NULL)
+        full = true;
+    else
+        full = false;
+    
+    free(pt);
+    return full;
+}
+
+//返回结点的数目
+unsigned int ListitemCount(const List *plist){
+    unsigned int count = 0;
+    Node * pnode = *plist;
+    
+    while (pnode != NULL){
+        count++;
+        pnode = pnode->next;
     }
-    menu();
     
-    //清理
-    EmptyTheList(&student);
-    printf("Bye.\n");
-    
-    return 0;
-    
-    
+    return count;
 }
 
-void showstudent(Item item)
-{
-    printf("name: %s, score: %d, Student Number: %d\n", item.name, item.score, item.num);
-}
-char * s_gets(char *st, int n)
-{
-    char *ret_val;
-    char *find;
+//创建储存项的节点，并将其添加至plist指向的链表的末尾
+bool AddItem(Item item, List *plist){
+    Node * pnew;
+    Node * scan = *plist;
     
-    ret_val = fgets(st, n, stdin);
-    if (ret_val)
+    pnew = (Node *)malloc(sizeof(Node));
+    if (pnew == NULL)
+        return false;//失败时退出函数
+    
+    CopyToNode(item, pnew);
+    pnew->next = NULL;
+    if (scan == NULL)   //如果链表为空链表
+        *plist = pnew;  //*pnew放在链表的开头
+    else
     {
-        find = strchr(st, '\n');
-        if (find)
-        *find = '\0';
-    else
-        while (getchar() != '\n')
-            continue;
-    }
-    return ret_val;
-}
-
-void menu(void){
-    puts("\n\n");
-    printf("\t\t\t----Student information management system----\n");
-    printf("\t\t\t--- (1)Menu              (2)AddStudent   ----\n");
-    printf("\t\t\t--- (3)DeleteStudent     (4)Updata       ----\n");
-    printf("\t\t\t--- (5)ShowStudents      (6)serchStudent ----\n");
-    printf("\t\t\t---------------------------------------------\n");
-    printf("choose what you want:\n");
-    int n;
-    scanf("%d", &n);
-    while(getchar() != '\n')
-        continue;
-    
-        if(n<=6&&n>=1){
-            switch(n){
-                case 1:menu();
-                case 2:Add();
-                case 3:Delete(&student);
-                case 4:Updet(&student);
-                case 5:show();
-                case 6:Serch(&student);
-                case 7:break;
-            }
-        }
-}
-
-void Add(void){
-    //获取用户输入并保存
-    Item temp;
-    puts("Enter student's name:");
-    while(s_gets(temp.name, TSIZE) != NULL && temp.name[0] != '\0'){
-        puts("Enter his'/her's score:");
-        scanf("%d", &temp.score);
-        puts("Enter his'/her number:");
-        scanf("%ud", &temp.num);
-        while(getchar() != '\n')
-            continue;
-        if(AddItem(temp, &student) == false){
-            fprintf(stderr, "Problem allocating memory\n");
-            break;
-        }
-        if(ListIsFull(&student)){
-            puts("The list is full now.");
-            break;
-        }
-        puts("Now, enter next student's name and score:(empty line to quit)");
-    }
-    puts("OK.");
-    menu();
-}
-
-void Delete(List *plist){
-    
-    if(ListIsEmpty(&student)){
-        puts("No memory to be deleted.");
-        return;
+        while (scan->next != NULL)
+            scan = scan->next;  //找到链表的末尾
+        scan->next = pnew;
     }
     
-    puts("Enter the StudentNumber you want to delete:");
-    unsigned int numdel;
-    scanf("%ud", &numdel);
+    return true;
+}
+
+//访问每个节点并执行 pfun指向的函数
+void Traverse (const List *plist, void (*pfun)(Item item))
+{
+    Node * pnode = *plist;//设置链表的开始
     
-    Node *pnode;
-    Node *prev = *plist;
-    pnode = prev->next;
-    while(pnode && pnode->item.num != numdel && prev->item.num != numdel){
-        prev = prev->next;
+    while (pnode != NULL)
+    {
+        (*pfun)(pnode->item);//把函数应用于链表中的项
         pnode = pnode->next;
     }
-    if(!pnode)  //如果链表里没有这个人
-        puts("No such person.");
-    else if(prev->item.num == numdel)   //链表头节点是要找的人
-        plist = NULL;
-    else
-        prev->next = pnode->next;
-    menu();
-    
 }
 
-void Serch(List *plist){
-    if(ListIsEmpty(plist))
-        puts("No memory to avilable;");
-    Node *pnode;
-    pnode = *plist;
-    int n;
+//释放有malloc()分配的内存
+//设置链表指针为NULL
+void EmptyTheList (List * plist)
+{
+    Node * psave;
     
-    puts("Enter his\\her num:");
-    scanf("%ud",&n);
-    while (pnode->item.num != n && pnode) {
-        pnode = pnode->next;
+    while (*plist != NULL)
+    {
+        psave = (*plist)->next;         //保存下一个节点的地址
+        free(*plist);                   //释放当前节点
+        *plist = psave;
     }
-    if(pnode)
-        showstudent(pnode->item);
-    else
-        puts("No such person.");
-    
-    menu();
 }
 
-void Updet(List *plist){
+static void CopyToNode(Item item, Node *pnode)
+{
+    pnode->item = item;//拷贝结构
     
-    
-}
-
-void show(void){
-    //显示
-    if(ListIsEmpty(&student))
-        printf("No data entered.");
-    else{
-        printf("Here is the students list:\n");
-        Traverse(&student, showstudent);
-    }
-    printf("You entered %d students.\n", ListitemCount(&student));
-    
-    return;
 }
