a15fd2a8abac71c6eef962f6e1ad4afd7cc2ecac
xuhuan lu <102048940+huanhuan0728@users.noreply.github.com>
2023-02-13 12:27:13 +0800

    Create favorite_languages2.py

---
+++ python/favorite_languages2.py
@@ -0,0 +1,20 @@
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil', 'sarah']
+
+for name in favorite_languages:
+    print(f"Hi, {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}")
+
